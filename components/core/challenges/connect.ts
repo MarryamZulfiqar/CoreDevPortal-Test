73cfa2b819922d7d736256a99db3850bca18a244
MarryamZulfiqar <13319389+MarryamZulfiqar@users.noreply.github.com>
2024-09-07 14:11:25 +0500

    Update connect.ts

--- components/core/challenges/connect.ts
+++ components/core/challenges/connect.ts
@@ -4,24 +4,32 @@ declare let window: {
   ethereum: ethers.providers.ExternalProvider;
 };
 
-const connect = async () => {
   try {
     const provider = undefined;
+
     if (provider) {
-      await undefined;
-      const signer = undefined;
-      const address = undefined;
-      return {
-        address,
-      };
+      // Request the currently selected accounts in MetaMask
+      const accounts = undefined;
+
+      // Make sure you have accounts and get the first one (currently selected)
+      if (accounts.length > 0) {
+        const address = undefined;
+        return {
+          address,
+        };
+      } else {
+        return {
+          error: 'No accounts found. Please select an account in MetaMask.',
+        };
+      }
     } else {
       return {
-        error: 'Please install Metamask at https://metamask.io',
+        error: 'Please install MetaMask at https://metamask.io',
       };
     }
   } catch (error) {
     return {
-      error: 'An unexpected error occurs',
+      error: 'An unexpected error occurred',
     };
   }
 };
